c789cae21ed6a5faad0c5f34a6ab4e34408c8aa8
Paulina Jędrzejewska <p.jedrzejewska97@gmail.com>
2021-03-09 19:17:06 +0100

    mongodb fix

--- app.js
+++ app.js
@@ -136,10 +136,6 @@ app.get("/about", function(req, res){
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started");
 });
